172bd73fc882fae7f02bba3e247ec6982f72d52d
jorge <jorge97fernandez@gmail.com>
2019-11-03 15:34:02 +0100

    Added languaje tests

--- src/test/java/translator/service/TranslatorServiceTest.java
+++ src/test/java/translator/service/TranslatorServiceTest.java
@@ -20,9 +20,20 @@ public class TranslatorServiceTest {
   TranslatorService translatorService;
 
   @Test
-  public void translateTest() {
+  public void translateTest_en_es() {
     TranslatedText translatedText = translatorService.translate("en", "es", "This is a test of translation service");
     assertEquals("Esto es una prueba de servicio de traducción", translatedText.getTranslation());
   }
 
+  @Test
+  public void translateTest_es_eu() {
+    TranslatedText translatedText = translatorService.translate("es", "eu", "Muchas gracias");
+    assertEquals("Eskerrik asko", translatedText.getTranslation());
+  }
+
+  @Test
+  public void translateTest_es_it() {
+    TranslatedText translatedText = translatorService.translate("es", "it", "Esto es otra prueba");
+    assertEquals("Questo è un altro test", translatedText.getTranslation());
+  }
 }
